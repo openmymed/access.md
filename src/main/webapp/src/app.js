78b25f1c706edb6c105566b47aff70d9002bff44
Tareq Kirresh <kirreshtareq@gmail.com>
2020-04-02 21:13:03 +0300

    updated gitignore, added redom JSX

--- src/main/webapp/src/app.js
+++ src/main/webapp/src/app.js
@@ -1,5 +1,33 @@
-import { router, mount, el, text} from 'redom'
-import './style/app.css'
-import './style/bootstrap.min.css'
-import '@fortawesome/fontawesome-free/js/all'
-import {App, goto} from 'redom-app'
+import { el, text, mount } from 'redom';
+class A{
+  constructor(attr, text) {
+    <div this="el">
+      <h3>{attr.title}</h3>
+      <span this="span">Hello</span> {text}
+      <h4>{attr.name}</h4>
+    </div>;
+  }
+  update(data){
+    this.span.textContent = data;
+  }
+}
+
+class B{
+  constructor() {
+    <div this="el">
+      <A this="a" title="Hello World example" name="A VERY TESTY BOI">
+        <span this="span">World</span>
+      </A>
+    </div>;
+  }
+  update() {
+    this.span.textContent = "You";
+    this.a.update("test");
+  }
+}
+
+const b = <B />;
+
+mount(document.body, b);
+
+b.update();
