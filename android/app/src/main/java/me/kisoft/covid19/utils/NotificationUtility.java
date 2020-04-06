51d6f8b496d9abe7d00ccb3483191dedb614756e
WaleedAbu-Nafiseh <43027952+WaleedAbu-Nafiseh@users.noreply.github.com>
2020-04-06 19:03:34 +0300

    Formatting

--- android/app/src/main/java/me/kisoft/covid19/utils/NotificationUtility.java
+++ android/app/src/main/java/me/kisoft/covid19/utils/NotificationUtility.java
@@ -1,27 +1,34 @@
 package me.kisoft.covid19.utils;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
-
+import androidx.core.app.NotificationManagerCompat;
 import me.kisoft.covid19.MainActivity;
 import me.kisoft.covid19.R;
 
-public class NotificationUtility extends AppCompatActivity {
-    private final int REQUEST_LOCATION_PERMISSION = 1;
+import static me.kisoft.covid19.AppWraper.CHANNEL_1_ID;
 
-    private static final String CHANNEL_ID ="1" ;
+public class NotificationUtility extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+
     Context context;
     public NotificationUtility(Context c) {
         this.context=c;
-
+        notificationManager = NotificationManagerCompat.from(c);
     }
-    public void setNotification(Context context, String notificationTitle, String notificationMessage, int notificationRequestCode){
+    public void notify(Context c, String notificationTitle, String notificationMessage, int notificationRequestCode){
+
+        setNotification(c, notificationTitle,  notificationMessage,  notificationRequestCode);
+        newNotification(c,notificationTitle,notificationMessage);
+    }
+
+    private void setNotification(Context context, String notificationTitle, String notificationMessage, int notificationRequestCode){
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(R.drawable.ic_notifications_black_24dp)
@@ -38,5 +45,16 @@ public class NotificationUtility extends AppCompatActivity {
         manager.notify(0, builder.build());
     }
 
+    public void newNotification(Context c,String notificationTitle, String notificationMessage ){
 
+        Notification notification = new NotificationCompat.Builder(c, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationMessage)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
 }
