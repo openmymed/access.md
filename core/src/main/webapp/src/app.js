0bcb14dd9af50fd760b36dcb8cce032808937212
Tareq Kirresh <kirreshtareq@gmail.com>
2020-04-13 01:09:26 +0300

    added fontawesome

--- core/src/main/webapp/src/app.js
+++ core/src/main/webapp/src/app.js
@@ -4,6 +4,7 @@ import { PatientList } from "./view/patient-list";
 import { PatientDetails } from "./view/patient-details";
 import { Signin } from "./view/signin";
 import { Home } from "./view/home";
+import '@fortawesome/fontawesome-free/js/all'
 import "./style/bootstrap.min.css";
 import "./style/app.css";
 import * as $ from "jquery";
