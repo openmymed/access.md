e47354995fe95745679fc0c3c002bb7e219ae09b
Majed Nuseibeh <majednuseibeh@gmail.com>
2020-04-13 03:28:57 +0300

    created home page

--- core/src/main/webapp/src/app.js
+++ core/src/main/webapp/src/app.js
@@ -4,7 +4,7 @@ import { PatientList } from "./view/patient-list";
 import { PatientDetails } from "./view/patient-details";
 import { Signin } from "./view/signin";
 import { Home } from "./view/home";
-import '@fortawesome/fontawesome-free/js/all'
+import "@fortawesome/fontawesome-free/js/all";
 import "./style/bootstrap.min.css";
 import "./style/app.css";
 import * as $ from "jquery";
@@ -14,11 +14,11 @@ class AuthenticationMiddleware {
   constructor() {}
   //this is what the auth function needs to look like
   exec(currentView, nextView, params) {
-    if (window.auth == false) {
-      return "signin";
-    } else {
-      return nextView;
-    }
+    // if (window.auth == false) {
+    //   return "signin";
+    // } else {
+    //   return nextView;
+    // }
   }
 }
 
@@ -26,7 +26,7 @@ const app = new App()
   .routes({
     home: Home,
     default: Signin,
-    signin: Signin,
+    default: Home,
     patients: PatientList,
     patient: PatientDetails,
   })
--- core/src/main/webapp/src/view/home.js
+++ core/src/main/webapp/src/view/home.js
@@ -8,192 +8,70 @@ import { PatientUpdates } from "../component/patient-updates";
 import { el, text, mount } from "redom";
 export class Home {
   constructor(attr, text) {
-    <div class="container-fluid display-table">
-      <div class="row display-table-row">
-        <div
-          class="col-md-2 col-sm-1 hidden-xs display-table-cell v-align box"
-          id="navigation"
-        >
-          <div class="logo">
-            <a hef="home.html">Covid Dashboard</a>
-          </div>
-          <div class="navi">
-            <ul>
-              <li class="active">
-                <a href="#">
-                  <i class="fa fa-home" aria-hidden="true"></i>
-                  <span class="hidden-xs hidden-sm">Home</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-tasks" aria-hidden="true"></i>
-                  <span class="hidden-xs hidden-sm">Workflow</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-bar-chart" aria-hidden="true"></i>
-                  <span class="hidden-xs hidden-sm">Statistics</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-user" aria-hidden="true"></i>
-                  <span class="hidden-xs hidden-sm">Calender</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-calendar" aria-hidden="true"></i>
-                  <span class="hidden-xs hidden-sm">Users</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-cog" aria-hidden="true"></i>
-                  <span class="hidden-xs hidden-sm">Setting</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-10 col-sm-11 display-table-cell v-align">
-          {/* <!--<button type="button" class="slide-toggle">Slide Toggle</button> --> */}
+    <nav this="el" class="navbar navbar-expand-md navbar-light">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="myNavBar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="myNavBar">
+        <div class="container-fluid">
           <div class="row">
-            <header>
-              <div class="col-md-7">
-                <nav class="navbar-default pull-left">
-                  <div class="navbar-header">
-                    <button
-                      type="button"
-                      class="navbar-toggle collapsed"
-                      data-toggle="offcanvas"
-                      data-target="#side-menu"
-                      aria-expanded="false"
-                    >
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                    </button>
-                  </div>
-                </nav>
-                <div class="search hidden-xs hidden-sm">
-                  <input type="text" placeholder="Search" id="search"></input>
-                </div>
-              </div>
-              <div class="col-md-5">
-                <div class="header-rightside">
-                  <ul class="list-inline header-top pull-right">
-                    <li class="hidden-xs">
-                      <a
-                        href="#"
-                        class="add-project"
-                        data-toggle="modal"
-                        data-target="#add_project"
-                      >
-                        Add Project
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i class="fa fa-envelope" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="icon-info">
-                        <i class="fa fa-bell" aria-hidden="true"></i>
-                        <span class="label label-primary">3</span>
-                      </a>
-                    </li>
-                    <li class="dropdown">
-                      <a
-                        href="#"
-                        class="dropdown-toggle"
-                        data-toggle="dropdown"
-                      >
-                        <img src="" alt="user" />
-                        <b class="caret"></b>
-                      </a>
-                      <ul class="dropdown-menu">
-                        <li>
-                          <div class="navbar-content">
-                            <span>JS Krishna</span>
-                            <p class="text-muted small">email goes here</p>
-                            <div class="divider"></div>
-                            <a href="#" class="view btn-sm active">
-                              View Profile
-                            </a>
-                          </div>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </header>
-          </div>
-          <div class="user-dashboard">
-            <h1>Hello, JS</h1>
-            <div class="row">
-              <div class="col-md-5 col-sm-5 col-xs-12 gutter">
-                <div class="sales">
-                  <h2>Your Sale</h2>
-
-                  <div class="btn-group">
-                    <button
-                      class="btn btn-secondary btn-lg dropdown-toggle"
-                      type="button"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <span>Period:</span> Last Year
-                    </button>
-                    <div class="dropdown-menu">
-                      <a href="#">2012</a>
-                      <a href="#">2014</a>
-                      <a href="#">2015</a>
-                      <a href="#">2016</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-7 col-sm-7 col-xs-12 gutter">
-                <div class="sales report">
-                  <h2>Report</h2>
-                  <div class="btn-group">
-                    <button
-                      class="btn btn-secondary btn-lg dropdown-toggle"
-                      type="button"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <span>Period:</span> Last Year
-                    </button>
-                    <div class="dropdown-menu">
-                      <a href="#">2012</a>
-                      <a href="#">2014</a>
-                      <a href="#">2015</a>
-                      <a href="#">2016</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <SideBar></SideBar>
+            <NavBar></NavBar>
           </div>
         </div>
       </div>
-    </div>;
-
-    // <div this="el">
-    // <h4>HOME</h4>
-    //     <Overview/>
-    //     <PatientUpdates/>
-    // </div>
+    </nav>;
   }
 
   update(data) {}
+}
+
+class NavBar {
+  constructor() {
+    <div this="el" class="col-lg-9"></div>;
+  }
+}
+
+class SideBar {
+  constructor() {
+    <div this="el" class="col-lg-3 sidebar fixed-bottom">
+      <a
+        href="#"
+        class="navbar-brand text-white d-block mx-auto text-center py-3"
+      >
+        Covid Dashboard
+      </a>
+      <ul class="navbar-nav flex-column">
+        <li class="nav-item">
+          <a href="#" class="sidebar-link nav-link p-3 mb-2">
+            <i class="fa fa-home fa-lg mr-3"></i>Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="sidebar-link nav-link p-3 mb-2">
+            <i class="fa fa-user fa-lg mr-3"></i>Patient List
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="sidebar-link nav-link p-3 mb-2">
+            <i class="fa fa-calendar-alt fa-lg mr-3"></i>Questions
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="sidebar-link nav-link p-3 mb-2">
+            <i class="fa fa-cog fa-lg mr-3"></i>Settings
+          </a>
+        </li>
+      </ul>
+      <button type="button" class="btn btn-primary ml-5 btn-lg">
+        <i class="fa fa-plus fa-lg mr-3"></i>
+        Add Patient
+      </button>
+    </div>;
+  }
 }
