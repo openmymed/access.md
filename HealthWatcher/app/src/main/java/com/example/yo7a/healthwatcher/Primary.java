30fbdf4a23e07425540977049199ef239b8b7a4b
WaleedAbu-Nafiseh <43027952+WaleedAbu-Nafiseh@users.noreply.github.com>
2020-04-24 22:45:21 +0300

    Import

---
+++ HealthWatcher/app/src/main/java/com/example/yo7a/healthwatcher/Primary.java
@@ -0,0 +1,105 @@
+package com.example.yo7a.healthwatcher;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Primary extends AppCompatActivity {
+
+    private String user;
+    private int p;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primary);
+
+        ImageButton HeartRate = this.findViewById(R.id.HR);
+        ImageButton BloodPressure = this.findViewById(R.id.BP);
+        ImageButton Ox2 = this.findViewById(R.id.O2);
+        ImageButton RRate = this.findViewById(R.id.RR);
+        ImageButton VitalSigns = this.findViewById(R.id.VS);
+        ImageButton Abt = this.findViewById(R.id.About);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            user = extras.getString("Usr");
+            //The key argument here must match that used in the other activity
+        }
+
+        Abt.setOnClickListener(v -> {
+            Intent i = new Intent(v.getContext(), AboutApp.class);
+            startActivity(i);
+            finish();
+        });
+
+
+        //Every Test Button sends the username + the test number, to go to the wanted test after the instructions activity
+        HeartRate.setOnClickListener(v -> {
+            p = 1;
+            Intent i = new Intent(v.getContext(), StartVitalSigns.class);
+            i.putExtra("Usr", user);
+            i.putExtra("Page", p);
+            startActivity(i);
+            finish();
+        });
+
+        BloodPressure.setOnClickListener(v -> {
+            p = 2;
+            Intent i = new Intent(v.getContext(), StartVitalSigns.class);
+            i.putExtra("Usr", user);
+            i.putExtra("Page", p);
+            startActivity(i);
+            finish();
+        });
+
+        RRate.setOnClickListener(v -> {
+            p = 3;
+            Intent i = new Intent(v.getContext(), StartVitalSigns.class);
+            i.putExtra("Usr", user);
+            i.putExtra("Page", p);
+            startActivity(i);
+            finish();
+        });
+
+        Ox2.setOnClickListener(v -> {
+            p = 4;
+            Intent i = new Intent(v.getContext(), StartVitalSigns.class);
+            i.putExtra("Usr", user);
+            i.putExtra("Page", p);
+            startActivity(i);
+            finish();
+
+        });
+
+        VitalSigns.setOnClickListener(v -> {
+            p = 5;
+            Intent i = new Intent(v.getContext(), StartVitalSigns.class);
+            i.putExtra("Usr", user);
+            i.putExtra("Page", p);
+            startActivity(i);
+            finish();
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, (arg0, arg1) -> {
+
+                    Primary.super.onBackPressed();
+                    finish();
+                    System.exit(0);
+                }).create().show();
+    }
+
+
+}
+
