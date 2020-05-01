f18f0ff9ed83fad863b7012cb793ad12434374cf
Majed Nuseibeh <majednuseibeh@gmail.com>
2020-05-01 20:03:52 +0300

    made view changes

--- android/app/src/main/java/me/kisoft/covid19/SettingsActivity.java
+++ android/app/src/main/java/me/kisoft/covid19/SettingsActivity.java
@@ -1,11 +1,11 @@
 package me.kisoft.covid19;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -16,7 +16,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        getSupportActionBar().setTitle(R.string.title_settings);
         btnChangeLanguage = findViewById(R.id.btn_change_language);
         btnChangePassword = findViewById(R.id.btn_change_password);
 
