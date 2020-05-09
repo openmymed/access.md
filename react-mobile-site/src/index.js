96392274fe517e6c297b77d0b6b59b3437ee36b3
Tareq Kirresh <kirreshtareq@gmail.com>
2020-05-10 00:49:17 +0300

    removed react mobile site

--- react-mobile-site/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
