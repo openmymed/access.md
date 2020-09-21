17a8bd8679ad56ba98f8f18035ad8a9671b44ab8
Tareq Kirresh <kirreshtareq@gmail.com>
2020-09-21 22:20:49 +0300

    restore-redom

---
+++ core/src/main/webapp/build/precache-manifest.4197404d304466050470f4dfec05fe31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a5b0d62b21648d4b7d9cb548dee87a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81a4cbd91605e9a13555",
+    "url": "/static/css/2.10713152.chunk.css"
+  },
+  {
+    "revision": "407c9db346b156417c5c",
+    "url": "/static/css/main.e14c0dd9.chunk.css"
+  },
+  {
+    "revision": "81a4cbd91605e9a13555",
+    "url": "/static/js/2.5a421e7d.chunk.js"
+  },
+  {
+    "revision": "dad249cda9bb5d20de96a375d5a8b5de",
+    "url": "/static/js/2.5a421e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407c9db346b156417c5c",
+    "url": "/static/js/main.5c58e674.chunk.js"
+  },
+  {
+    "revision": "e4585e9545bc218f71b6",
+    "url": "/static/js/runtime-main.95134277.js"
+  },
+  {
+    "revision": "5d8d15114f5e9eb4ec476b32f31b86cb",
+    "url": "/static/media/openmymed.5d8d1511.png"
+  }
+]);
