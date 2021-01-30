2d7d8133ee7b34e602e12d0fcd398df015b8c333
Tareq Kirresh <kirreshtareq@gmail.com>
2021-01-31 01:56:30 +0200

    updated xpath queries

--- core/src/test/java/org/openmymed/test/android/page/LoginActivity.java
+++ core/src/test/java/org/openmymed/test/android/page/LoginActivity.java
@@ -5,24 +5,23 @@
  */
 package org.openmymed.test.android.page;
 
+import static com.codeborne.selenide.Selenide.$;
 import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.By;
 
 /**
  *
  * @author tareq
  */
 public class LoginActivity {
-
-    @AndroidFindBy(xpath = "//*[contains(@id, 'et_username')]")
-    private SelenideElement loginField;
     
-    @AndroidFindBy(xpath= "//*[contains(@id, 'tv_password')]")
-    private SelenideElement passwordField;
+    private SelenideElement loginField = $(By.xpath("//*[contains(@id, 'et_username')]"));
     
-    @AndroidFindBy(xpath="//*[contains(@id, 'btn_sign_in')]")
-    private SelenideElement signinButton;
-
+    private SelenideElement passwordField = $(By.xpath("//*[contains(@id, 'tv_password')]"));
+    
+    private SelenideElement signinButton = $(By.xpath("//*[contains(@id, 'btn_sign_in')]"));
+    
     public MainActivity login(String username, String password) {
         loginField.val(username);
         passwordField.val(password);
@@ -30,11 +29,11 @@ public class LoginActivity {
         return new MainActivity();
     }
     
-    public LoginActivity then(){
+    public LoginActivity then() {
         return this;
     }
     
-    public LoginActivity get(){
+    public LoginActivity get() {
         return this;
     }
 }
