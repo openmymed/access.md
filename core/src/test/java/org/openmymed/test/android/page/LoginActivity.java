31c0a0f0338e05696584c417961df278ae24a0b8
Tareq Kirresh <kirreshtareq@gmail.com>
2021-01-31 02:07:06 +0200

    updated selectors

--- core/src/test/java/org/openmymed/test/android/page/LoginActivity.java
+++ core/src/test/java/org/openmymed/test/android/page/LoginActivity.java
@@ -7,32 +7,30 @@ package org.openmymed.test.android.page;
 
 import static com.codeborne.selenide.Selenide.$;
 import com.codeborne.selenide.SelenideElement;
-import io.appium.java_client.pagefactory.AndroidFindBy;
-import org.openqa.selenium.By;
 
 /**
  *
  * @author tareq
  */
 public class LoginActivity {
-    
-    private SelenideElement loginField = $(By.xpath(".//*[contains(@id, 'et_username')]"));
-    
-    private SelenideElement passwordField = $(By.xpath(".//*[contains(@id, 'tv_password')]"));
-    
-    private SelenideElement signinButton = $(By.xpath(".//*[contains(@id, 'btn_sign_in')]"));
-    
+
+    private SelenideElement loginField = $("#@id/et_username");
+
+    private SelenideElement passwordField = $("#@id/et_password");
+
+    private SelenideElement signinButton = $("#@id/btn_sign_in");
+
     public MainActivity login(String username, String password) {
         loginField.val(username);
         passwordField.val(password);
         signinButton.click();
         return new MainActivity();
     }
-    
+
     public LoginActivity then() {
         return this;
     }
-    
+
     public LoginActivity get() {
         return this;
     }
